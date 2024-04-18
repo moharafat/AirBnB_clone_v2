75a367ec775e4748963100d5a0c3ae7be41701de
Mohamed Amr <mohamedamr0001@.com>
2024-04-18 04:58:40 +0200

    REVIEW FILE

--- models/review.py
+++ models/review.py
@@ -2,9 +2,15 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
 
+from models.base_model import BaseModel, Base
+from models import how_to_store
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = ' reviews'
+    text = Column(String(1024), nullable=False)
+
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),nullable=False)
