1ea2b5fdf5f0895f32f9f554807c1b19973d5f17
Mohamed Amr <mohamedamr0001@.com>
2024-04-18 23:22:01 +0200

    if getenv("HBNB_TYPE_STORAGE") == "db":

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,7 @@
 
 from os import getenv
 
-how_to_store = getenv('HBNB_TYPE_STORAGE', 'db')
-
-if how_to_store == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 
