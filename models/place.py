650ce8b2f13754220cdf912fb6c323081de0a142
Mohamed Amr <mohamedamr0001@.com>
2024-04-18 04:48:55 +0200

    places

--- models/place.py
+++ models/place.py
@@ -2,28 +2,20 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models import how_to_store
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'users'
-    if how_to_store == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship(
-                'Place',
-                backref='user',
-                cascade='all, delete, delete-orphan')
-        reviews = relationship(
-                'Review',
-                backref='user',
-                cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    __tablename__ = 'Place'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
