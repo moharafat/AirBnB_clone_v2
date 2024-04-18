6958be9564e62cfd50ca6f0260eaae6bc5c26a29
Sarah-Gad <sarahgad298@gmail.com>
2024-04-17 20:02:39 -0700

    .-.

--- models/city.py
+++ models/city.py
@@ -14,6 +14,10 @@ class City(BaseModel, Base):
     if how_to_store == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship(
+                'Place',
+                backref='cities',
+                cascade='all, delete, delete-orphan')
     else:
         name = ''
         state_id = ''
