ab683faf845c95a78ee14d4f7706ead22ec8d73d
Sarah-Gad <sarahgad298@gmail.com>
2024-04-17 19:24:09 -0700

    ^^

--- models/place.py
+++ models/place.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import how_to_store
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = 'users'
+    if how_to_store == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+                'Place',
+                backref='user',
+                cascade='all, delete, delete-orphan')
+        reviews = relationship(
+                'Review',
+                backref='user',
+                cascade='all, delete, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
