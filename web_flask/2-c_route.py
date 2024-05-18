6283ab7429714cbd2be91dcfef3628a5a2b70971
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-18 18:23:35 +0300

    text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """
+    returns Hello HBNB
+    """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """
+    returns HBNB
+    """
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text():
+    """
+    Replace underscores with spaces
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
