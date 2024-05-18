73130d52a583c53a96f659788b3b58731047b576
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-18 17:12:29 +0300

    chmod

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/, strict_slashes=False")
+
+def Hello_HBNB():
+    return ("Hello HBNB!")
+
+if __name__ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
+
+git config --global user.name "Mohamed Arafat"
+git config --global user.email "mohamedamr0001@gmail.com"
