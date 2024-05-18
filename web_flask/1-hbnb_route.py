288cf7181cf52930c844bfca2b29b6d5e83ca446
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-18 18:16:04 +0300

    yo

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """
+    returns Hello HBNB
+    """
+
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """
+    returns Hello HBNB
+    """
+
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
