11272bbcfab27e5067016c543f8e6bfa03ed3ac2
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-19 15:01:22 +0300

    ad

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,7 +65,3 @@ class FileStorage:
         which_obj = obj.to_dict()['__class__'] + '.' + obj.id
         if which_obj in self.__objects.keys():
             del self.__objects[which_obj]
-    
-    def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
-        self.reload()
